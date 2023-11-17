b7d5473737bd41936e7279202fad2a4ab302bcf7
f30318acer <f50318acer@gmail.com>
2023-11-17 17:31:59 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/MembersController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjMusicBetter.Controllers
+{
+    public class MembersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+    }
+}
