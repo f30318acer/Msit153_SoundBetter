9ab6139092edc8c4c8f33657aa0175f2b07575ae
f30318acer <f50318acer@gmail.com>
2023-11-20 12:05:45 +0800

    update
    update

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -34,7 +34,16 @@ namespace WebApplication1.Controllers
         {
             return View();
         }
-		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult NoLogin()
+        {
+            return View();
+        }
+        public IActionResult ForgetPwd()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
--- WebApplication1/Controllers/MembersController.cs
+++ WebApplication1/Controllers/MembersController.cs
@@ -12,5 +12,13 @@ namespace prjMusicBetter.Controllers
         {
             return View();
         }
+        public IActionResult Profile1()
+        {
+            return View();
+        }
+        public IActionResult CoolPon()
+        {
+            return View();
+        }
     }
 }
