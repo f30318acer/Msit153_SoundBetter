7942b446584c84faafcaabe381a91f3f7ef6c19a
Liaocheyu <Liaocheyu61@gmail.com>
2023-11-20 09:44:16 +0800

    加入專案檔案。

---
+++ WebApplication1/Controllers/Blogv1Controller.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjMusicBetter.Controllers
+{
+	public class Blogv1Controller : Controller
+	{
+		public IActionResult IList()
+		{
+			return View();
+		}
+	}
+}
