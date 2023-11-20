40d263cbd6a01dd78cb85ba20f037acebdcc4ef5
Liaocheyu <Liaocheyu61@gmail.com>
2023-11-20 15:08:24 +0800

    //

---
+++ WebApplication1/Controllers/ArticleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjMusicBetter.Controllers
+{
+	public class ArticleController : Controller
+	{
+		public IActionResult List()
+		{
+			return View();
+		}
+	}
+}
--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=ShoppingCart}/{action=List}/{id?}");
+    pattern: "{controller=Article}/{action=List}/{id?}");
 
 app.Run();
