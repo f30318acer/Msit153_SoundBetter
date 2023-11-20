a4ca3f794d362fa3e205a18b79b706cde6afe54d
f30318acer <f50318acer@gmail.com>
2023-11-20 16:50:35 +0800

    update
    update

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=BgHome}/{action=Index}/{id?}");
+    pattern: "{controller=BgMember}/{action=Index}/{id?}");
 
 app.Run();
