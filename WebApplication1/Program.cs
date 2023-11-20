1b7ca574555003a0cc441e3a98c6d98898188614
jonson0975 <jonson50815@yahoo.com.tw>
2023-11-20 16:33:56 +0800

    Update

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Place}/{action=PlaceManage}/{id?}");
+    pattern: "{controller=Place}/{action=BgPlaceManage}/{id?}");
 
 app.Run();
