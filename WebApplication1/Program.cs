0d4279fd521d48aeb79c3e61e8ad712785e80967
acespare <asfsjj123@gmail.com>
2023-11-21 10:41:08 +0800

    Update Program.cs

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Place}/{action=PlaceManage}/{id?}");
+    pattern: "{controller=Rate}/{action=Rate}/{id?}");
 
 app.Run();
