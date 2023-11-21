0d6646d9cd1ea9cdcb793ec140f637eb5ac8ccc3
acespare <asfsjj123@gmail.com>
2023-11-21 11:51:23 +0800

    create Productcontroller
    create Productcontroller

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace prjMusicBetter.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
