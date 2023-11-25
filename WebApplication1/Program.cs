5f3c3cb297c6aab28857a2dac6c50f6dd7904071
jonson0975 <jonson50815@yahoo.com.tw>
2023-11-25 12:16:25 +0800

    update
    update

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -30,6 +30,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=TSites}/{action=Index}/{id?}");
 
 app.Run();
