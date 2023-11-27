3b6c1d5e649a46e13c93205fa3f64dbff77411ca
jonson0975 <jonson50815@yahoo.com.tw>
2023-11-27 16:46:40 +0800

    3
    3

--- WebApplication1/Controllers/ClassController.cs
+++ WebApplication1/Controllers/ClassController.cs
@@ -24,11 +24,24 @@ namespace prjMusicBetter.Controllers
 
 
 
-		/*=======課程內頁===============*/
+		/*=======新增課程===============*/
 
 		public IActionResult Createclass()
 		{
 			return View();
 		}
-	}
+
+        [HttpPost]
+        public IActionResult Createclass(TProduct p)
+        {
+            DbDemoContext db = new DbDemoContext();
+            db.TProducts.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("List");
+
+
+            // 若是 Required(ErrorMessage = "名稱是必填欄位") 顯示錯誤
+            // 則把jquery版本更新至最新 < script src = "~/Scripts/jquery-3.4.1.min.js" >
+        }
+    }
 }
