ad275f3025dce9e48ff1d80bac099939fd4124f0
jonson0975 <jonson50815@yahoo.com.tw>
2023-11-28 17:24:16 +0800

    update

--- WebApplication1/Models/ViewModels/SiteViewModel.cs
+++ WebApplication1/Models/ViewModels/SiteViewModel.cs
@@ -6,5 +6,18 @@ namespace prjMusicBetter.Models.ViewModels
     {
         public TSite? TSite { get; set; }
         public TSitePicture? TSitePicture { get; set; }
+        public string SiteTypeText
+        {
+            get
+            {
+                var siteTypeMapping = new Dictionary<int, string>
+                {
+                    { 1, "音樂學校" },
+                    { 2, "錄音室" },
+                };
+
+                return siteTypeMapping.GetValueOrDefault(TSite?.FSiteType ?? 0, "未知");
+            }
+        }
     }
 }
