34f084a737ce45fe1ad2dcb6a22c21faeab1e1d0
ChunLiLin <charles03292@gmail.com>
2023-11-29 10:55:02 +0800

    //

---
+++ WebApplication1/Controllers/apiTStyleController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using prjSoundBetterApi.Models;
+
+namespace prjSoundBetterApi.Controllers
+{
+    public class apiTStyleController : Controller
+    {
+        private readonly dbSoundBetterContext _context;
+        public apiTStyleController(dbSoundBetterContext context)
+        {
+            _context = context;
+        }
+        //===List_All===
+        public IActionResult List()
+        {
+            var dbSoundBetterContext = _context.TStyles;
+            return Json(dbSoundBetterContext);
+        }
+    }
+}
