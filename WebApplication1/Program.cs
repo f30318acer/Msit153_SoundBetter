01ea8701af76599aeca2626b1c4a0fc2382f46a6
acespare <asfsjj123@gmail.com>
2023-11-29 17:01:48 +0800

    新增結帳頁面
    新增結帳頁面

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Product}/{action=Create}/{id?}");
+    pattern: "{controller=ShoppingCart}/{action=Checkout}/{id?}");
 
 app.Run();
