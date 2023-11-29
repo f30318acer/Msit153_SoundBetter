6d2f7d9521242afa2cbe6fe7b9f6fb9b5aa91918
f30318acer <f50318acer@gmail.com>
2023-11-29 17:53:00 +0800

    update
    update

--- WebApplication1/Models/ViewModels/FMemberVM.cs
+++ WebApplication1/Models/ViewModels/FMemberVM.cs
@@ -4,8 +4,15 @@ namespace prjMusicBetter.Models.ViewModels
 {
     public class FMemberVM
     {
+        //畫面上顯示甚麼這裡就會出現甚麼
+        //MemberDao 那裏也要出現資料，但是有些不會在註冊畫面顯示的則會在那邊寫死
         public string? fPhotoPath {  get; set; }
         public IFormFile? Photo {  get; set; }
+
+        [Display(Name = "用戶者姓名")]
+        [Required(ErrorMessage = "{0}是必填欄位!")]
+        [DataType(DataType.Text)]
+        public string fUsername { get; set; }
 
         [Display(Name = "姓名")]
         [Required(ErrorMessage ="{0}是必填欄位!")]
