8f2ebdb821f893bbcbc439586103680793ed3bf1
f30318acer <f50318acer@gmail.com>
2023-12-06 15:26:54 +0800

    update
    update

--- WebApplication1/Models/ViewModels/FriendsViewModel.cs
+++ WebApplication1/Models/ViewModels/FriendsViewModel.cs
@@ -1,9 +1,10 @@
-﻿namespace prjMusicBetter.Models
+﻿namespace prjMusicBetter.Models.ViewModels
 {
     internal class FriendsViewModel
     {
         public TMember Member { get; set; }
         public List<TMember> Friends { get; set; }
+        public List<TMember> BlackList { get; set; }
 
     }
 }
