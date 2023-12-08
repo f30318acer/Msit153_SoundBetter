eabef9aa60b8b5320bcf6a6efc9abf509503c731
Liaocheyu <Liaocheyu61@gmail.com>
2023-12-08 16:03:23 +0800

    1
    1

--- WebApplication1/Controllers/ArticleController.cs
+++ WebApplication1/Controllers/ArticleController.cs
@@ -18,8 +18,18 @@ namespace prjMusicBetter.Controllers
             _context = context;
             _userInfoService = userInfoService;
         }
-        public IActionResult List()
+        public IActionResult List3()
         {
+            TMember member = _userInfoService.GetMemberInfo();
+            if (member == null)
+            {
+                // 處理會員資料不存在的情況，導向登入頁面
+                return RedirectToAction("Login", "Home"); // 導向登入頁面
+            }
+            if (member != null)
+            {
+                ViewBag.MemberId = member.FMemberId;
+            }
             return View();
         }
         public IActionResult Detail2()
