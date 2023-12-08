437ba9de38c96fb41d84d0c6af56fd601c77c2a6
f30318acer <f50318acer@gmail.com>
2023-12-08 13:52:17 +0800

    update
    update

--- WebApplication1/Models/dbSoundBetterContext.cs
+++ WebApplication1/Models/dbSoundBetterContext.cs
@@ -95,9 +95,6 @@ public partial class dbSoundBetterContext : DbContext
 
     public virtual DbSet<TWorkType> TWorkTypes { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=192.168.22.199;Initial Catalog=dbSoundBetter;Persist Security Info=True;User ID=bbb;Password=123");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
