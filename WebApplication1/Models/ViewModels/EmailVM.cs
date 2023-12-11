9057d7dce9c56ad3e06cb1544ee785e9648784b6
jonson0975 <jonson50815@yahoo.com.tw>
2023-12-11 13:28:48 +0800

    update

---
+++ WebApplication1/Models/ViewModels/EmailVM.cs
@@ -0,0 +1,10 @@
+﻿namespace prjMusicBetter.Models.ViewModels
+{
+    public class EmailVM
+    {
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Subject { get; set; }
+        public string? Message { get; set; }
+    }
+}
