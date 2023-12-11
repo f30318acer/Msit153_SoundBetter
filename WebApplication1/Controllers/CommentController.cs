027711475875bf9db2f2154fcb9ec4298882a158
Quentin Hong <bc780902@gmail.com>
2023-12-11 14:54:18 +0800

    1211

--- WebApplication1/Controllers/CommentController.cs
+++ WebApplication1/Controllers/CommentController.cs
@@ -1,18 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using prjMusicBetter.Models;
 using prjMusicBetter.Models.Dtos.Comment;
+using prjMusicBetter.Models.infra;
 using prjMusicBetter.Models.ViewModels;
+using System.Xml.Linq;
 
 namespace prjMusicBetter.Controllers
 {
     public class CommentController : Controller
     {
         private readonly dbSoundBetterContext _context;
+        private readonly UserInfoService _userInfoService;
 
-        public CommentController(dbSoundBetterContext context)
+        public CommentController(dbSoundBetterContext context, UserInfoService userInfoService)
         {
             _context = context;
+            _userInfoService = userInfoService;
         }
 
         public IActionResult Index()
@@ -22,7 +27,10 @@ namespace prjMusicBetter.Controllers
 
         public IActionResult List(int articleId)
         {
-            //取得_當前文章所有留言資料
+            //確認會員是否有登入
+            TMember member = _userInfoService.GetMemberInfo();
+            if (member == null) { return RedirectToAction("Login", "Home"); }
+            //取得_文章所有留言資料
             articleId = 20;
             var comments = _context.TComments.Include(e => e.FMember).Where(e => e.FArticleId == articleId).ToList();
             var viewModel = new CommentListViewModel()
@@ -31,12 +39,17 @@ namespace prjMusicBetter.Controllers
             };
 
             return View(viewModel);
+
         }
 
+
+        public IActionResult Create()
+        { return View(); }
         [HttpPost]
         public IActionResult Create(CommentDto dto)
         {
-            //新增_文章備註資料
+
+            dto.ArticleId = 20;
             _context.TComments.Add(new TComment()
             {
                 FMemberId = dto.MemberId,
@@ -47,6 +60,46 @@ namespace prjMusicBetter.Controllers
             _context.SaveChanges();
 
             return Ok();
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            dbSoundBetterContext db = new dbSoundBetterContext();
+            TComment x = db.TComments.FirstOrDefault(p => p.FCommentId == id);
+            if (x != null)
+                return RedirectToAction("List");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(TComment pIN)
+        {
+            dbSoundBetterContext db = new dbSoundBetterContext();
+            TComment pDB = db.TComments.FirstOrDefault(p => p.FCommentId == pIN.FCommentId);
+            if (pDB != null)
+            {
+                pDB.FMemberId = pIN.FMemberId;
+                pDB.FArticleId = pIN.FArticleId;
+                pDB.FCommentContent = pIN.FCommentContent;
+                pIN.FCommentTime = DateTime.Now;
+
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
+
+
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            dbSoundBetterContext db = new dbSoundBetterContext();
+            TComment x = db.TComments.FirstOrDefault(p => p.FCommentId == id);
+            if (x != null)
+            {
+                db.TComments.Remove(x);
+                db.SaveChanges();
+            }
+            return RedirectToAction("List");
         }
     }
 }
