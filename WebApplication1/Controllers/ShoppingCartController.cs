a88dcb16cbcebf9c968f817a6b0b5e5247ba35ee
acespare <asfsjj123@gmail.com>
2023-12-11 13:46:46 +0800

    fix

--- WebApplication1/Controllers/ShoppingCartController.cs
+++ WebApplication1/Controllers/ShoppingCartController.cs
@@ -6,19 +6,19 @@ using prjMusicBetter.Models.ViewModels;
 
 namespace prjMusicBetter.Controllers
 {
-	public class ShoppingCartController : Controller
-	{
+    public class ShoppingCartController : Controller
+    {
         private readonly UserInfoService _userInfoService;
         private readonly dbSoundBetterContext _context;
 
-        public ShoppingCartController (UserInfoService userInfoService, dbSoundBetterContext dbSoundBetterContext)
+        public ShoppingCartController(UserInfoService userInfoService, dbSoundBetterContext dbSoundBetterContext)
         {
             _userInfoService = userInfoService;
             _context = dbSoundBetterContext;
         }
 
-		public IActionResult List()
-		{
+        public IActionResult List()
+        {
             // 取得Session
             int memberId = _userInfoService.GetMemberId();
             var cart = HttpContext.Session.Get<List<ShoppingCartVM>>($"ShoppingCart_{memberId}") ?? new List<ShoppingCartVM>();
@@ -26,7 +26,7 @@ namespace prjMusicBetter.Controllers
             cart = checkShoppingCart(cart);
 
             return View(cart);
-		}
+        }
 
         /// <summary>
         /// 點選確認購買 至驗證頁
@@ -71,7 +71,8 @@ namespace prjMusicBetter.Controllers
             {
                 // 購物車內Class資料更新，檢查待補
                 var classData = _context.TClasses.FirstOrDefault(m => m.FClassId == item.ProductId);
-                if (classData != null)
+                
+                if (classData != null )
                 {
                     item.ProductName = classData.FClassName;
                     item.ProductPrice = classData.FPrice;
@@ -79,6 +80,7 @@ namespace prjMusicBetter.Controllers
                     item.ProductThumbnailPath = classData.FThumbnailPath;
                     item.ProductStartDate = classData.FStartdate;
                     item.ProductEndDate = classData.FEnddate;
+                   
                     // 如果出現售完、已刪除等等
                     if (classData.FCurrentStudent >= classData.FMaxStudent)
                     {
@@ -86,11 +88,12 @@ namespace prjMusicBetter.Controllers
                     }
                     item.ProductStatus = 0000;
                 }
-            }
+                
 
+
+            }
             return cart;
         }
-       
     }
 
 }
