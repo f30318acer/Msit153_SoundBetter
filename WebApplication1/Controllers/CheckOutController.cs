56116139cabc2712d74a47e07070e728bec6b596
f30318acer <f50318acer@gmail.com>
2023-12-14 01:57:24 +0800

    1
    1

--- WebApplication1/Controllers/CheckOutController.cs
+++ WebApplication1/Controllers/CheckOutController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using prjMusicBetter.Models;
+using Stripe.Checkout;
+
 
 namespace prjMusicBetter.Controllers
 {
@@ -14,18 +16,109 @@ namespace prjMusicBetter.Controllers
                 {
                     Product = "Tommy Hilfiger",
                     Rate=1500,
-                    Quanity=2,
+                    Quantity=2,
                     ImagePath="img/Image1.jpg"
                 },
                  new ProductEntity
                 {
                     Product = "TimeWear",
                     Rate=1000,
-                    Quanity=1,
+                    Quantity=1,
                     ImagePath="img/Image2.jpg"
                 },
             };
             return View(productList);
+        }
+
+
+        public IActionResult OrderConfirmation()
+        {
+            var service = new SessionService();
+            Session session = service.Get(TempData["Session"].ToString());
+
+
+            if (session.PaymentStatus == "paid")
+            {
+                var transaction = session.PaymentIntentId.ToString();
+
+                return View("Success");
+            }
+            return View("Login");
+
+        }
+        public IActionResult Success()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult CheckOut()
+        {
+            List<ProductEntity> productList = new List<ProductEntity>();
+            productList = new List<ProductEntity>
+            {
+                new ProductEntity
+                {
+                    Product = "Tommy Hilfiger",
+                    Rate=1500,
+                    Quantity=2,
+                    ImagePath="img/Image1.jpg"
+                },
+                 new ProductEntity
+                {
+                    Product = "TimeWear",
+                    Rate=1000,
+                    Quantity=1,
+                    ImagePath="img/Image2.jpg"
+                },
+                  new ProductEntity
+                {
+                    Product = "Timex",
+                    Rate=300,
+                    Quantity=2,
+                    ImagePath="img/Image2.jpg"
+                }
+            };
+
+            var domain = "http://localhost:7078/";
+
+            var options = new SessionCreateOptions
+            {
+                SuccessUrl = domain + $"CheckOut/OrderConfirmation",//付款完出現
+                CancelUrl = domain + "CheckOut/Login",
+                LineItems = new List<SessionLineItemOptions>(),
+                Mode = "payment",
+                CustomerEmail = "f50318acer@gmail.com"
+
+            };
+            foreach (var item in productList)
+            {
+                var sessionListItem = new SessionLineItemOptions
+                {
+                    PriceData = new SessionLineItemPriceDataOptions()
+                    {
+                        UnitAmount = (long)(item.Rate * item.Quantity),
+                        Currency = "inr",
+                        ProductData = new SessionLineItemPriceDataProductDataOptions
+                        {
+                            Name = item.Product.ToString(),
+
+                        }
+                    },
+                    Quantity = item.Quantity,
+                };
+                options.LineItems.Add(sessionListItem);
+            }
+            var service = new SessionService();
+            Session session = service.Create(options);
+
+            TempData["Session"] = session.Id;
+
+            Response.Headers.Add("Location", session.Url);
+            return new StatusCodeResult(303);
+
         }
     }
 }
--- WebApplication1/Models/ProductEntity.cs
+++ WebApplication1/Models/ProductEntity.cs
@@ -1,11 +1,12 @@
-﻿namespace prjMusicBetter.Models
+﻿
+namespace prjMusicBetter.Models
 {
     public class ProductEntity
     {
         public int Id { get; set; }
         public string Product { get; set; }
         public long Rate {  get; set; }
-        public long Quanity {  get; set; }
+        public long Quantity {  get; set; }
         public string ImagePath {  get; set; }
     }
 }
