5006d753fd58945e3442479d6273c4a2caf03494
f30318acer <f50318acer@gmail.com>
2023-12-14 00:33:41 +0800

    update
    update

---
+++ WebApplication1/Controllers/CheckOutController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using prjMusicBetter.Models;
+
+namespace prjMusicBetter.Controllers
+{
+    public class CheckOutController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<ProductEntity> productList = new List<ProductEntity>();
+            productList = new List<ProductEntity>
+            {
+                new ProductEntity
+                {
+                    Product = "Tommy Hilfiger",
+                    Rate=1500,
+                    Quanity=2,
+                    ImagePath="img/Image1.jpg"
+                },
+                 new ProductEntity
+                {
+                    Product = "TimeWear",
+                    Rate=1000,
+                    Quanity=1,
+                    ImagePath="img/Image2.jpg"
+                },
+            };
+            return View(productList);
+        }
+    }
+}
