29608d5710a3eb68f80fc83bf815183bf0c3ea08
f30318acer <f50318acer@gmail.com>
2023-12-15 17:50:16 +0800

    u
    u

--- WebApplication1/wwwroot/js/ClassClick.js
+++ WebApplication1/wwwroot/js/ClassClick.js
@@ -1,14 +1,16 @@
 ﻿$(document).ready(function () {
     $.ajax({
-        url: '/Chart/GetChartData', // 替换为您的实际 URL
+        url: '/Chart/GetChartData', // 确保这是正确的URL
         method: 'GET',
+        contentType: 'application/json', // 发送的数据类型
+        dataType: 'json', // 期望的响应类型
         success: function (chartData) {
             const labels = chartData.map(item => item.ClassName);
             const data = chartData.map(item => item.TotalClicks);
 
-            const ctx = document.getElementById('myChart');
-            new Chart(ctx, {
-                type: 'bar',
+            const ctx = document.getElementById('myChart').getContext('2d');
+            const myChart = new Chart(ctx, {
+                type: 'bar', // 您可以在这里改为 'line' 如果您想要折线图
                 data: {
                     labels: labels,
                     datasets: [{
@@ -22,20 +24,32 @@
                 options: {
                     scales: {
                         y: {
-                            beginAtZero: true
+                            beginAtZero: true,
+                            ticks: {
+                                stepSize: 1 // 根据您数据的范围调整
+                            }
                         }
                     },
                     legend: {
-                        display: true
+                        display: true,
+                        position: 'top' // 图例位置
                     },
                     responsive: true,
-                    maintainAspectRatio: false
+                    maintainAspectRatio: false,
+                    tooltips: {
+                        mode: 'index',
+                        intersect: false
+                    },
+                    hover: {
+                        mode: 'nearest',
+                        intersect: true
+                    }
                 }
             });
         },
-        error: function (error) {
-            // 處理錯誤
-            console.error("發生錯誤:", error);
+        error: function (xhr, status, error) {
+            // 更详细的错误处理
+            console.error("AJAX请求失败:", xhr, status, error);
         }
     });
 });
