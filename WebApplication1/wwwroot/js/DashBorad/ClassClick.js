f4728dd425c9a6a86dfe7fd785d80aa043c90224
f30318acer <f50318acer@gmail.com>
2023-12-15 15:04:16 +0800

    h
    h

--- WebApplication1/wwwroot/js/DashBorad/ClassClick.js
+++ WebApplication1/wwwroot/js/DashBorad/ClassClick.js
@@ -1,77 +1,42 @@
-﻿Highcharts.chart('container1', {
+﻿$(document).ready(function () {
+    // 從伺服器端獲取數據
+    $.ajax({
+        url: '@Url.Action("GetClassClicksData", "Chart")', // 請將 YourControllerName 替換為實際的控制器名稱
 
-    title: {
-        text: 'U.S Solar Employment Growth',
-        align: 'left'
-    },
+        type: 'GET',
 
-    subtitle: {
-        text: 'By Job Category. Source: <a href="https://irecusa.org/programs/solar-jobs-census/" target="_blank">IREC</a>.',
-        align: 'left'
-    },
+        success: function (data) {
+            // 創建 Highcharts 圖表
+            Highcharts.chart('container', {
+                chart: {
+                    type: 'line'
+                },
+                title: {
+                    text: '課程點擊次數'
+                },
+                xAxis: {
+                    categories: data.map(item => item.className),
+                    title: {
+                        text: '課程名稱'
+                    }
+                },
+                yAxis: {
+                    title: {
+                        text: '點擊次數'
+                    }
+                },
+                series: [{
+                    name: '點擊次數',
+                    data: data.map(item => item.totalClicks)
+                }]
+                // ... 其他配置 ...
+            });
+        },
+        error: function (xhr, status, error) {
+            // 處理錯誤
+            console.error('Error fetching data: ', error);
 
-    yAxis: {
-        title: {
-            text: 'Number of Employees'
         }
-    },
-
-    xAxis: {
-        accessibility: {
-            rangeDescription: 'Range: 2010 to 2020'
-        }
-    },
-
-    legend: {
-        layout: 'vertical',
-        align: 'right',
-        verticalAlign: 'middle'
-    },
-
-    plotOptions: {
-        series: {
-            label: {
-                connectorAllowed: false
-            },
-            pointStart: 2010
-        }
-    },
-
-    series: [{
-        name: 'Installation & Developers',
-        data: [43934, 48656, 65165, 81827, 112143, 142383,
-            171533, 165174, 155157, 161454, 154610]
-    }, {
-        name: 'Manufacturing',
-        data: [24916, 37941, 29742, 29851, 32490, 30282,
-            38121, 36885, 33726, 34243, 31050]
-    }, {
-        name: 'Sales & Distribution',
-        data: [11744, 30000, 16005, 19771, 20185, 24377,
-            32147, 30912, 29243, 29213, 25663]
-    }, {
-        name: 'Operations & Maintenance',
-        data: [null, null, null, null, null, null, null,
-            null, 11164, 11218, 10077]
-    }, {
-        name: 'Other',
-        data: [21908, 5548, 8105, 11248, 8989, 11816, 18274,
-            17300, 13053, 11906, 10073]
-    }],
-
-    responsive: {
-        rules: [{
-            condition: {
-                maxWidth: 500
-            },
-            chartOptions: {
-                legend: {
-                    layout: 'horizontal',
-                    align: 'center',
-                    verticalAlign: 'bottom'
-                }
-            }
-        }]
-    }
-
+    });
 });
+
