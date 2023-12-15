89a015a02d9ba746687327522053a579914d092b
Quentin Hong <bc780902@gmail.com>
2023-12-15 16:32:56 +0800

    12151630

---
+++ WebApplication1/Controllers/CommentController.cs
@@ -0,0 +1,119 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using prjMusicBetter.Models;
+using prjMusicBetter.Models.Dtos.Comment;
+using prjMusicBetter.Models.infra;
+using prjMusicBetter.Models.ViewModels;
+using System.Xml.Linq;
+
+namespace prjMusicBetter.Controllers
+{
+    public class CommentController : Controller
+    {
+        private readonly dbSoundBetterContext _context;
+        private readonly UserInfoService _userInfoService;
+        private IWebHostEnvironment _environment = null;
+
+        public CommentController(dbSoundBetterContext context, UserInfoService userInfoService, IWebHostEnvironment environment)
+        {
+            _context = context;
+            _userInfoService = userInfoService;
+            _environment = environment;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List(int articleId)
+        {
+            //確認會員是否有登入
+            TMember member = _userInfoService.GetMemberInfo();
+            if (member == null) { return RedirectToAction("Login", "Home"); }
+            //取得_文章所有留言資料
+            articleId = 20;
+            var comments = _context.TComments.Include(e => e.FMember).Where(e => e.FArticleId == articleId).ToList();
+            var viewModel = new CommentListViewModel()
+            {
+                Comments = comments,
+            };
+
+            return View(viewModel);
+
+        }
+
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(CommentDto dto)
+        {
+
+            dto.FArticleId = 20;
+            //dto.Content = "test123";
+
+            _context.TComments.Add(new TComment()
+            {
+                //FMemberId = dto.MemberId,
+                FCommentContent = dto.FCommentContent,
+                FArticleId = dto.FArticleId,
+                FCommentTime = DateTime.Now,
+            });
+            _context.SaveChanges();
+
+            return Ok();
+        }
+  
+
+        public IActionResult Edit(int? id)
+        {
+
+            TComment comment = _context.TComments.FirstOrDefault(p => p.FCommentId == id);
+            if (comment != null)
+                return RedirectToAction("List");
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Edit(TComment pIN)
+        {
+
+            TComment pDB = _context.TComments.FirstOrDefault(p => p.FCommentId == pIN.FCommentId);
+            if (pDB != null)
+            {
+                pDB.FArticleId = pIN.FArticleId;
+                pDB.FCommentContent = pIN.FCommentContent;
+                pDB.FMemberId = pIN.FMemberId;
+                pDB.FCommentTime = DateTime.Now;
+                _context.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+
+
+
+        public IActionResult Delete(int? id)
+        {
+            TComment comment = _context.TComments.FirstOrDefault(p => p.FCommentId == id);
+            if (comment != null)
+            {
+                _context.TComments.Remove(comment);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("List");
+        }
+
+
+
+
+        private bool TCommentExitsts(int id)
+        {
+            return (_context.TComments?.Any(e => e.FCommentId == id)).GetValueOrDefault();
+        }
+
+
+    }
+}
--- WebApplication1/Models/ViewModels/CommentListViewModel.cs
+++ WebApplication1/Models/ViewModels/CommentListViewModel.cs
@@ -1,7 +1,15 @@
-﻿namespace prjMusicBetter.Models.ViewModels
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace prjMusicBetter.Models.ViewModels
 {
     public class CommentListViewModel
     {
         public List<TComment> Comments { get; set; }
+
+        public List<TArticle> Articles { get; set; }
+
+        public int SelectedFArticleId { get; set; }
+
+        public SelectList FArticleIdOptions { get; set; }
     }
 }
