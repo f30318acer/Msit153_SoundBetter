7862a3a89a466e81ae9c32d8e03dfa3ed62eabe1
f30318acer <f50318acer@gmail.com>
2023-12-16 15:22:12 +0800

    u
    u

--- WebApplication1/Controllers/ChartController.cs
+++ WebApplication1/Controllers/ChartController.cs
@@ -25,7 +25,17 @@ namespace prjMusicBetter.Controllers
         public IActionResult classclick()
         {
             return View();
-        }    
+        }
+        public IActionResult workclick()
+        {
+            return View();
+        }
+        public IActionResult CombinedClicks()
+        {
+            return View("CombinedClicks");
+
+        }
+      
         public IActionResult GenderRatio()
         {
 
@@ -40,33 +50,55 @@ namespace prjMusicBetter.Controllers
             return Json(genderCount);
         }
         [HttpPost]
-        public List<object> GetClassClicksData()
+        public IActionResult GetClassClicksData()
+        {
+            // 首先，從 TClasses 表中選擇所有類別名稱和ID
+            var classInfo = _context.TClasses
+                .Select(c => new { c.FClassId, c.FClassName })
+                .ToList();
+
+            // 接著，從 TClassClick 表中選擇類別ID和對應的點擊次數
+            var clickInfo = _context.TClassClicks
+                .GroupBy(cc => cc.FClassId)
+                .Select(g => new { FClassId = g.Key, FClick = g.Sum(x => x.FClick) })
+                .ToList();
+
+            // 然後，將這兩個列表聯接在一起，以 FClassId 為聯接條件
+            var classClicksData = classInfo
+                .Join(clickInfo,
+                    c => c.FClassId,
+                    cc => cc.FClassId,
+                    (c, cc) => new
+                    {
+                        c.FClassName,
+                        cc.FClick
+                    })
+                .ToList();
+
+            // 最後，將結果轉換為您需要的格式，這裡假設您需要兩個分開的列表
+            List<object> data = new List<object>
+    {
+        classClicksData.Select(x => x.FClassName).ToList(),
+        classClicksData.Select(x => x.FClick).ToList()
+    };
+
+            return Json(data); // 將數據包裝成 JSON 格式返回
+
+
+        }
+        [HttpPost]
+        public List<object> GetWorksClicksData()
         {
             List<object> data = new List<object>();
 
-            List<string> classes = _context.TClasses.Select(p=>p.FClassName).ToList();
+            List<string> works = _context.TWorks.Select(w=>w.FWorkName).ToList();
 
-            data.Add(classes);
+            data.Add(works);
 
-            List<int> SalesClass = _context.TClasses.Select(p => p.FClassId).ToList();
-            data.Add(SalesClass);
+            List<int> ClicksWorks = _context.TWorks.Select(c =>c.FClick).ToList();
+            data.Add(ClicksWorks);
             return data;
 
-            //var classClicksData = _context.TClasses
-            //    .Join(_context.TClassClicks,
-            //          c => c.FClassId,
-            //          cc => cc.FClassId,
-            //          (c, cc) => new { c.FClassName, cc.FClick })
-            //    .GroupBy(x => x.FClassName)
-            //    .Select(group => new
-            //    {
-            //        ClassName = group.Key,
-            //        TotalClicks = group.Sum(x => x.FClick)
-            //    })
-            //    .OrderBy(x => x.ClassName)
-            //    .ToList();
-
-            //return Json(classClicksData);
         }
 
     }
