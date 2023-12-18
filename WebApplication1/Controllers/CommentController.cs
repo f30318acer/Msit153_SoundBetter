41302af3d8e10e49cd4971b280c203c67526b4e7
Quentin Hong <bc780902@gmail.com>
2023-12-18 13:39:40 +0800

    文章評論留言管理更新到後台
    文章評論留言管理更新到後台 首頁原本連結改成/Chat/index

--- WebApplication1/Controllers/CommentController.cs
+++ WebApplication1/Controllers/CommentController.cs
@@ -28,27 +28,63 @@ namespace prjMusicBetter.Controllers
         }
 
         //取得_文章所有留言資料
-        //
-        //
 
-        public IActionResult List(int articleId, int commentId)
+
+
+        //public IActionResult List(int? articleId, int commentId)
+        //{
+        //    確認會員是否有登入
+        //    TMember member = _userInfoService.GetMemberInfo();
+        //    if (member == null) { return RedirectToAction("Login", "Home"); }
+
+
+
+        //    articleId = 20;
+        //    var comments = _context.TComments
+        //                .Include(e => e.FMember)
+        //                .Where(e => e.FArticleId == articleId.Value).ToList();
+        //    .Include(e => e.FCommentId)
+        //    .Where(e => e.FCommentId == commentId).ToList();
+        //    var viewModel = new CommentListViewModel()
+        //    {
+        //        Comments = comments,
+        //    };
+
+        //    return View(viewModel);
+
+
+
+        //}
+
+        public IActionResult List(int? articleId)
         {
-            //確認會員是否有登入
-            TMember member = _userInfoService.GetMemberInfo();
-            if (member == null) { return RedirectToAction("Login", "Home"); }
 
-            articleId = 20;
-            var comments = _context.TComments
-                            .Include(e => e.FMember)
-                            .Where(e => e.FArticleId == articleId).ToList();
-                            //.Include(e => e.FCommentId)
-                            //.Where(e => e.FCommentId == commentId).ToList();
-            var viewModel = new CommentListViewModel()
-            {
-                Comments = comments,
-            };
+               TMember member = _userInfoService.GetMemberInfo();
+               if (member == null) { return RedirectToAction("Login", "Home"); }
+            //// ...
+            if (articleId==null)
+                {
+                // 如果articleId未提供，您可以採取適當的措施，例如返回錯誤信息或默認值。
+                // 預設頁面articleId=20;
+                articleId = 20;
+                 }
+            else 
+                { 
+                articleId = articleId.Value; 
+                 }
+                
+                // 使用articleId進行相應的處理
+                var comments = _context.TComments
+                    .Include(e => e.FMember)
+                    .Where(e => e.FArticleId == articleId.Value)
+                    .ToList();
 
-            return View(viewModel);
+                var viewModel = new CommentListViewModel()
+                {
+                    Comments = comments,
+                };
+
+                return View(viewModel);
 
         }
 
@@ -61,12 +97,23 @@ namespace prjMusicBetter.Controllers
         public IActionResult Create(CommentDto dto)
         {
 
-            dto.FArticleId = 20;
+            if (dto.FArticleId == null)
+            {
+                // 如果articleId未提供，您可以採取適當的措施，例如返回錯誤信息或默認值。
+                // 預設頁面articleId=20;
+                dto.FArticleId = 20;
+            }
+            else
+            {
+                dto.FArticleId = dto.FArticleId;
+            }
+
+            //dto.FArticleId = 20;
             //dto.Content = "test123";
 
             _context.TComments.Add(new TComment()
             {
-                //FMemberId = dto.MemberId,
+                FMemberId = dto.FMemberId,
                 FCommentContent = dto.FCommentContent,
                 FArticleId = dto.FArticleId,
                 FCommentTime = DateTime.Now,
@@ -103,7 +150,7 @@ namespace prjMusicBetter.Controllers
 
 
 
-        public IActionResult Delete(int? id)
+        public IActionResult Delete(int? id, int? articleId)
         {
             TComment comment = _context.TComments.FirstOrDefault(p => p.FCommentId == id);
             if (comment != null)
@@ -111,7 +158,7 @@ namespace prjMusicBetter.Controllers
                 _context.TComments.Remove(comment);
                 _context.SaveChanges();
             }
-            return RedirectToAction("List");
+            return RedirectToAction("List", new { articleId = articleId });
         }
 
 
