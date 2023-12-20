614cff8fb6dd2bd4c7611ea3e41b3201faa94be3
marshall4741 <allen.n.marshall@gmail.com>
2023-12-20 14:21:13 +0800

    後臺檢視資料

--- WebApplication1/Controllers/ChartController.cs
+++ WebApplication1/Controllers/ChartController.cs
@@ -65,6 +65,8 @@ namespace prjMusicBetter.Controllers
                 .ToList();
             return Json(genderCount);
         }
+
+
         [HttpPost]
         public IActionResult GetClassClicksData()
         {
@@ -93,26 +95,60 @@ namespace prjMusicBetter.Controllers
 
             // 最後，將結果轉換為您需要的格式，這裡假設您需要兩個分開的列表
             List<object> data = new List<object>
-    {
-        classClicksData.Select(x => x.FClassName).ToList(),
-        classClicksData.Select(x => x.FClick).ToList()
-    };
+            {
+                classClicksData.Select(x => x.FClassName).ToList(),
+                classClicksData.Select(x => x.FClick).ToList()
+            };
 
             return Json(data); // 將數據包裝成 JSON 格式返回
         }
+
+
         [HttpPost]
-        public List<object> GetWorksClicksData()
+        //public List<object> GetWorksClicksData()
+        public IActionResult GetWorksClicksData()
         {
-            List<object> data = new List<object>();
+            //List<object> data = new List<object>();
 
-            List<string> works = _context.TWorks.Select(w => w.FWorkName).ToList();
+            //List<string> works = _context.TWorks.Select(w => w.FWorkName).ToList();
 
-            data.Add(works);
+            //data.Add(works);
 
-            var ClicksWorks = _context.TWorks.Select(c => c.FClick).ToList();
-            data.Add(ClicksWorks);
-            return data;
+            //var ClicksWorks = _context.TWorks.Select(c => c.FClick).ToList();
+            //data.Add(ClicksWorks);
+            //return data;
 
+            // 首先，從 TClasses 表中選擇所有類別名稱和ID
+            var workInfo = _context.TWorks
+                .Select(c => new { c.FWorkId, c.FWorkName })
+                .ToList();
+
+            // 接著，從 TClassClick 表中選擇類別ID和對應的點擊次數
+            var clickInfo = _context.TWorks
+                .GroupBy(cc => cc.FWorkId)
+                .Select(g => new { FWorkId = g.Key, FClick = g.Sum(x => x.FClick) })
+                .ToList();
+
+            // 然後，將這兩個列表聯接在一起，以 FClassId 為聯接條件
+            var workClicksData = workInfo
+                .Join(clickInfo,
+                    c => c.FWorkId,
+                    cc => cc.FWorkId,
+                    (c, cc) => new
+                    {
+                        c.FWorkName,
+                        cc.FClick
+                    })
+                .ToList();
+
+            // 最後，將結果轉換為您需要的格式，這裡假設您需要兩個分開的列表
+            List<object> data = new List<object>
+            {
+                workClicksData.Select(x => x.FWorkName).ToList(),
+                workClicksData.Select(x => x.FClick).ToList()
+            };
+
+            return Json(data); // 將數據包裝成 JSON 格式返回
         }
         [HttpPost]
         public IActionResult GetProjectStatusData()
@@ -142,10 +178,10 @@ namespace prjMusicBetter.Controllers
 
             // 最後，將結果轉換為您需要的格式，這裡假設您需要兩個分開的列表
             List<object> data = new List<object>
-    {
-        ProjectStatusData.Select(x => x.FDescription).ToList(),
-        ProjectStatusData.Select(x => x.FMemberId).ToList()
-    };
+            {
+                ProjectStatusData.Select(x => x.FDescription).ToList(),
+                ProjectStatusData.Select(x => x.FMemberId).ToList()
+            };
 
             return Json(data); // 將數據包裝成 JSON 格式返回
 
@@ -179,10 +215,10 @@ namespace prjMusicBetter.Controllers
 
             // 最後，將結果轉換為您需要的格式，這裡假設您需要兩個分開的列表
             List<object> data = new List<object>
-    {
-        ProjectStatusData.Select(x => x.FDescription).ToList(),
-        ProjectStatusData.Select(x => x.FSiteId).ToList()
-    };
+            {
+                ProjectStatusData.Select(x => x.FDescription).ToList(),
+                ProjectStatusData.Select(x => x.FSiteId).ToList()
+            };
 
             return Json(data); // 將數據包裝成 JSON 格式返回
 
