b6dfb6b908eec75d8400bd9b27ef97ac339e1004
f30318acer <f50318acer@gmail.com>
2023-12-27 09:51:45 +0800

    u
    u

--- WebApplication1/wwwroot/js/dashboard/DashBorad.js
+++ WebApplication1/wwwroot/js/dashboard/DashBorad.js
@@ -1,35 +1,27 @@
 ﻿$(document).ready(function ()
 {
     // 從伺服器端獲取數據
+    // AJAX call to fetch data
     $.ajax({
         type: "GET",
-        url: '/Chart/GenderRatio', // 確保這是返回上述 JSON 數據的端點
+        url: '/Chart/GenderRatio', // Make sure this endpoint is correct and returns data
         success: function (data) {
-            // 使用數據建立餅圖
-            Highcharts.chart('container', {
+            // On successful data retrieval
+            Highcharts.chart('container', { // Make sure 'container' is the correct id
                 chart: {
                     type: 'pie',
-                     style: {
-                        fontSize: '20px' // 更改為所需的文字大小
-                    }
                 },
                 title: {
                     text: '會員性別比例',
                     style: {
-                        fontSize: '20px' // 更改為所需的文字大小
+                        fontSize: '20px'
                     }
                 },
-
                 tooltip: {
                     pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>',
                     style: {
                         fontSize: '15px'
                     },
-                },
-                accessibility: {
-                    point: {
-                        valueSuffix: '%'
-                    }
                 },
                 plotOptions: {
                     pie: {
@@ -39,7 +31,7 @@
                             enabled: true,
                             format: '<b>{point.name}</b>: {point.percentage:.1f} %',
                             style: {
-                                fontSize: '600px' // 將這個值改為你想要的大小
+                                fontSize: '15px' // Ensure this is a reasonable value
                             }
                         }
                     }
@@ -54,10 +46,11 @@
             });
         },
         error: function (error) {
-            // 處理錯誤
-            console.error("發生錯誤:", error);
+            // Handle errors here
+            console.error("Error fetching data:", error);
         }
     });
+
 });
 
 
